ab8e5f2892791b5316df0f1ac3faf86e708e39e7
Zuzana Kollarova <zuzana.kollarova@ivyassistant.com>
2023-03-03 12:57:44 +0100

    init

---
+++ laundrology/laundrology/laundrologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  laundrologyApp.swift
+//  laundrology
+//
+//  Created by Zuzana Kollarova on 03/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct laundrologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
